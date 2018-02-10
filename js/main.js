f709ef6bbf74288db64bbe3aa5d8d99ac889d9c8
Richard Spence <richardspence@Richards-MacBook-Air-2.local>
2018-02-09 16:26:38 -0800

    First Commit

---
+++ js/main.js
@@ -0,0 +1,64 @@
+
+
+//Persistent data structures for efficient immutablity
+
+/*
+ * Programming Quiz: Murder Mystery (3-4)
+ */
+
+// change the value of `room` and `suspect` to test your code
+var room = "ballroom";
+var suspect = "Mr. Parkes";
+
+var weapon = "";
+var solved = false;
+
+if (room === "dining room") {
+    weapon = "knife";
+    solved = true;
+    suspect = "Mr. Parkes";
+} else if (room === "gallery") {
+    weapon = "trophy"
+    solved = true;
+    suspect = "Ms. Van Cleve";
+} else if (room == "ballroom") {
+    weapon = "poison";
+    solved = true;
+    suspect = "Mr. Kalehoff";
+} else {
+    weapon = "pool stick";
+    solved = true;
+    suspect = "Mrs. Sparr";
+
+}
+
+if (solved) {
+	console.log(suspect + " did it in the " + room + " with the " + weapon + "!");
+}
+
+
+/*
+Functions as parameters
+Being able to store a function in a variable makes it really simple to pass the function into another function.
+A function that is passed into another function is called a callback.
+Let's say you had a helloCat() function,
+and you wanted it to return "Hello" followed by a string of "meows" like you had with catSays.
+Well, rather than redoing all of your hard work, you can make helloCat() accept a callback function, and pass in catSays.
+*/
+
+// function expression catSays
+var catSays = function(max) {
+  var catMessage = "";
+  for (var i = 0; i < max; i++) {
+    catMessage += "meow ";
+  }
+  return catMessage;
+};
+
+// function declaration helloCat accepting a callback
+function helloCat(callbackFunc) {
+  return "Hello " + callbackFunc(3);
+}
+
+// pass in catSays as a callback function
+helloCat(catSays);
